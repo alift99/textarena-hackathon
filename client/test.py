4d5e17798dc38b54e765f50a1212f1489d8a5810
Clarence <cl.sheng34@gmail.com>
2025-03-08 14:45:54 +0800

    chore: Merge

--- client/test.py
+++ client/test.py
@@ -1,25 +1,32 @@
 import textarena as ta
 
-from langchain_agent import OpenAILangchainAgent, ClaudeLangchainAgent
+from dotenv import load_dotenv
+from langchain_agent import OpenAILangchainAgent, ClaudeLangchainAgent, ClaudeLangchainAgentCustom
 from tools.spelling_bee_tool import find_valid_word
+from tools.simple_negotiation_tools import simple_negotiation_tools
 from system_prompts import SPELLINGBEE_PROMPT
+
+
+
+# Load API key from .env
+load_dotenv()
 
 # Initialize agents
 agents = {
     # 0: OpenAIReactAgent(model_name="gpt-4o-mini"),
-    0: ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest", system_prompt=SPELLINGBEE_PROMPT, tools=[find_valid_word]),
-    1: ta.agents.AnthropicAgent(model_name="claude-3-5-sonnet-latest"),
+    1: ClaudeLangchainAgentCustom(model_name="claude-3-5-sonnet-latest"),
+    0: ClaudeLangchainAgentCustom(model_name="claude-3-5-sonnet-latest", tools=simple_negotiation_tools),
 }
 
 # Initialize environment from subset and wrap it
-env = ta.make(env_id="SpellingBee-v0")
+env = ta.make(env_id="SimpleNegotiation-v0")
 env = ta.wrappers.LLMObservationWrapper(env=env)
 env = ta.wrappers.SimpleRenderWrapper(
     env=env,
     player_names={0: "Claude Agent", 1: "Claude"},
 )
 
-env.reset()
+env.reset(num_players=len(agents))
 done = False
 while not done:
     player_id, observation = env.get_observation()
