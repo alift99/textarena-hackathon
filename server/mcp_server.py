fad1a379b2e0fd785e7b25f2e1de2b9197514189
Alif Daffa <67675036+alift99@users.noreply.github.com>
2025-03-08 15:49:07 +0800

    refactor: improve and integrate spelling bee tool to mcp (#5)

--- server/mcp_server.py
+++ server/mcp_server.py
@@ -1,12 +1,28 @@
 from fastmcp import FastMCP
+from nltk.corpus import words
 
 
 # Create an MCP server
 mcp = FastMCP("Demo")
 
+valid_words = set(words.words())  # Load dictionary of words
 
 # Add an addition tool
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+def find_valid_word(char_list: list, n: int):
+    """Find the first valid word using exactly n characters from the given list."""
+
+    print(f'SpellingBee tool called: ```char_list: {char_list}, n: {n}```')
+
+    output = []
+    n_options = 10
+    # Iterate over valid_words and check if the word satisfies the conditions
+    for word in valid_words:
+        # Check if the word has exactly 'n' characters and only uses characters from 'char_list'
+        if len(word) == n and all(char in char_list for char in word):
+            output.append(word)
+            if len(output) >= n_options:
+                print(f'Tool output: {output}')
+                return output
+    print(f'Tool output: {output}')
+    return output  # No valid word found
