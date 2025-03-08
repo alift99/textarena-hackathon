b058008a0ce322a81078153af511dcb6b5f1b185
Shang Hong Sim <65533108+shanghongsim@users.noreply.github.com>
2025-03-08 14:33:37 +0800

    Negotiation agent

--- client/test.py
+++ client/test.py
@@ -1,30 +1,31 @@
 import os
-from dotenv import load_dotenv
 
 import textarena as ta
-
-from langchain_agent import OpenAILangchainAgent, ClaudeLangchainAgent
-
+from dotenv import load_dotenv
+from langchain_agent import (
+    ClaudeLangchainAgent,
+    ClaudeLangchainAgentCustom,
+    OpenAILangchainAgent,
+)
 
 # Load API key from .env
 load_dotenv()
 
 # Initialize agents
 agents = {
-    # 0: OpenAIReactAgent(model_name="gpt-4o-mini"),
     0: ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest"),
-    1: ta.agents.OpenAIAgent(model_name="gpt-4o-mini"),
+    1: ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest"),
 }
 
 # Initialize environment from subset and wrap it
-env = ta.make(env_id="SpellingBee-v0")
+env = ta.make(env_id="SimpleNegotiation-v0")
 env = ta.wrappers.LLMObservationWrapper(env=env)
 env = ta.wrappers.SimpleRenderWrapper(
     env=env,
-    player_names={0: "Player 1", 1: "Player 2"},
+    player_names={0: "Opponent", 1: "Self"},
 )
 
-env.reset()
+env.reset(num_players=len(agents))
 done = False
 while not done:
     player_id, observation = env.get_observation()
