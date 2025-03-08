8a3fd8f267c58c75417dce7b1f5968dcd56c5206
alift99 <alifdaffa.main@gmail.com>
2025-03-08 14:24:57 +0800

    feature: setup router agent

--- client/test.py
+++ client/test.py
@@ -1,14 +1,39 @@
 import textarena as ta
 
-from langchain_agent import OpenAILangchainAgent, ClaudeLangchainAgent
+from agents.langchain_agent import OpenAILangchainAgent, ClaudeLangchainAgent
+from agents.router_agent import RouterAgent
+
 from tools.spelling_bee_tool import find_valid_word
-from system_prompts import SPELLINGBEE_PROMPT
+
+from system_prompts import STANDARD_GAME_PROMPT, SPELLINGBEE_PROMPT
+
+
+general_agent = ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest", system_prompt=STANDARD_GAME_PROMPT, tools=[])
+spellingbee_agent = ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest", system_prompt=SPELLINGBEE_PROMPT, tools=[find_valid_word])
+
+models = [
+    {
+        'name': 'general_agent',
+        'description': 'An agent for general purpose',
+        'model': general_agent
+    }, 
+    {
+        'name': 'spellingbee_agent', 
+        'description': 'An agent specialized for spellingbee',
+        'model': spellingbee_agent
+    }
+]
+
+agent = RouterAgent(
+    model_name="claude-3-5-sonnet-latest",
+    models=models,
+)
 
 # Initialize agents
 agents = {
     # 0: OpenAIReactAgent(model_name="gpt-4o-mini"),
-    0: ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest", system_prompt=SPELLINGBEE_PROMPT, tools=[find_valid_word]),
-    1: ta.agents.AnthropicAgent(model_name="claude-3-5-sonnet-latest"),
+    0: agent,
+    1: general_agent,
 }
 
 # Initialize environment from subset and wrap it
