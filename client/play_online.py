ec293248ff7082b8e68803e1e01b2bc8e0fd1bdd
Alif Daffa <67675036+alift99@users.noreply.github.com>
2025-03-08 15:45:03 +0800

    feature: integrated poker agent into router (#4)

--- client/play_online.py
+++ client/play_online.py
@@ -1,35 +1,67 @@
 import textarena as ta
 
-from agents.langchain_agent import OpenAILangchainAgent, ClaudeLangchainAgent
+from agents.langchain_agent import ClaudeLangchainAgent
+from agents.router_agent import RouterAgent
+
 from tools.spelling_bee_tool import find_valid_word
-from system_prompts import SPELLINGBEE_PROMPT
+from tools.poker_odds_tool import poker_odds
+
+from system_prompts import STANDARD_GAME_PROMPT, SPELLINGBEE_PROMPT, POKER_PROMPT
  
 
-model_name = "test agent"
-model_description = "test agent"
+model_name = "HumanGPT"
+model_description = "A mixture of customized agents, orchestrated by a *router* agent"
 email = "alifdaffa.main@gmail.com"
 
 
-# Initialize agent
-agent = ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest", system_prompt=SPELLINGBEE_PROMPT, tools=[find_valid_word]) 
+general_agent = ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest", system_prompt=STANDARD_GAME_PROMPT, tools=[])
+spellingbee_agent = ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest", system_prompt=SPELLINGBEE_PROMPT, tools=[find_valid_word])
+poker_agent = ClaudeLangchainAgent(model_name="claude-3-5-sonnet-latest", system_prompt=POKER_PROMPT, tools=[poker_odds])
 
+models = [
+    {
+        'name': 'general_agent',
+        'description': 'An agent for general purpose',
+        'model': general_agent
+    }, 
+    {
+        'name': 'spellingbee_agent', 
+        'description': 'An agent specialized for spellingbee',
+        'model': spellingbee_agent
+    },
+    {
+        'name': 'poker_agent',
+        'description': 'An agent specialized for poker',
+        'model': poker_agent
+    }
+]
 
-env = ta.make_online(
-    env_id=["SpellingBee-v0"], 
-    model_name=model_name,
-    model_description=model_description,
-    email=email
+agent = RouterAgent(
+    model_name="claude-3-5-sonnet-latest",
+    models=models,
 )
-env = ta.wrappers.LLMObservationWrapper(env=env)
+
+n_games = 10
+
+for _ in range(n_games):
+    env = ta.make_online(
+        env_id=["SpellingBee-v0"], 
+        model_name=model_name,
+        model_description=model_description,
+        email=email
+    )
+    env = ta.wrappers.LLMObservationWrapper(env=env)
 
 
-env.reset(num_players=1)
+    env.reset(num_players=1)
 
-done = False
-while not done:
-    player_id, observation = env.get_observation()
-    action = agent(observation)
-    done, info = env.step(action=action)
+    done = False
+    while not done:
+        player_id, observation = env.get_observation()
+        action = agent(observation)
+        done, info = env.step(action=action)
 
-
-rewards = env.close()
+    try:
+        rewards = env.close()
+    except:
+        pass
