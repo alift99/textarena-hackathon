1b82b1cbda4ba2873f2aefa6da5bba5af751b955
alift99 <alifdaffa.main@gmail.com>
2025-03-08 10:46:23 +0800

    initialized repositories

---
+++ client/test.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+import json
+
+import textarena as ta
+
+from react_agent import OpenAIReactAgent, conversation_history
+
+
+# Load API key from .env
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+# Initialize agents
+agents = {
+    # 0: OpenAIReactAgent(model_name="gpt-4o-mini"),
+    0: ta.agents.OpenAIAgent(model_name="gpt-4o-mini"),
+    1: ta.agents.OpenAIAgent(model_name="gpt-4o-mini"),
+}
+
+# Initialize environment from subset and wrap it
+env = ta.make(env_id="SpellingBee-v0")
+env = ta.wrappers.LLMObservationWrapper(env=env)
+env = ta.wrappers.SimpleRenderWrapper(
+    env=env,
+    player_names={0: "Player 1", 1: "Player 2"},
+)
+
+env.reset()
+done = False
+while not done:
+    player_id, observation = env.get_observation()
+    action = agents[player_id](observation)
+    done, info = env.step(action=action)
+rewards = env.close()
